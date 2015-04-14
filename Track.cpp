382b237cd0fcd480985b2f08ee7a1489c08a791d
jgriordan <jriorda2@nd.edu>
2015-04-14 17:19:05 -0400

    added comments about track

--- Track.cpp
+++ Track.cpp
@@ -10,6 +10,8 @@ void Track::draw(){
 	glPushMatrix();
 	glTranslatef(-2.5,-1,0);
 	for( z=0; z<660; z++ ){
+
+		// determine colors for rainbow
 		col = ((float)(z%10))/10;
 		if( z%60 < 10 ){
 			glColor3f(1,col,0);
@@ -24,8 +26,11 @@ void Track::draw(){
 		}else{
 			glColor3f(1,0,1-col);
 		}
+
+		// initial straight section (120)
 		if( z<120 ){
 			for( x=0; x<10; x++ ){
+				// make checkered start/finish line
 				if( z == 0 ||  z == 2  ){
 					if( x % 2 )
 						glColor3f(0.9,0.9,0.9);
@@ -41,14 +46,20 @@ void Track::draw(){
 				glTranslatef(0.5,0,0);
 			}
 			glTranslatef(-5,0,0.5);
-		}else if( z<130 ){
+		}
+
+		// first turn (left,10)
+		else if( z<130 ){
 			glColor3f(0,0.5,0.5);
 			for( x=0; x<130-z; x++ ){
 				glutSolidCube(0.5);
 				glTranslatef(0.5,0,0);
 			}
 			glTranslatef(-0.5*(float)x+0.5,0,0.5);
-		}else if( z<210 ){
+		}
+
+		// straight section (80)
+		else if( z<210 ){
 			if( z == 130 )
 				glTranslatef(0,0,-0.5);
 			for( x=0; x<10; x++ ){
@@ -56,14 +67,20 @@ void Track::draw(){
 				glTranslatef(0,0,-0.5);
 			}
 			glTranslatef(0.5,0,5);
-		}else if( z<220 ){
+		}
+
+		// second turn (left,10)
+		else if( z<220 ){
 			glColor3f(0,0.5,0.5);
 			for( x=0; x<220-z; x++ ){
 				glutSolidCube(0.5);
 				glTranslatef(0,0,-0.5);
 			}
 			glTranslatef(0.5,0,0.5*(float)x-0.5);
-		}else if( z<460 ){
+		}
+
+		// straight section (240)
+		else if( z<460 ){
 			if( z == 220 )
 				glTranslatef(-0.5,0,0);
 			for( x=0; x<10; x++ ){
@@ -71,14 +88,20 @@ void Track::draw(){
 				glTranslatef(-0.5,0,0);
 			}
 			glTranslatef(5,0,-0.5);
-		}else if( z<470 ){
+		}
+
+		// third turn (left,10)
+		else if( z<470 ){
 			glColor3f(0,0.5,0.5);
 			for( x=0; x<470-z; x++ ){
 				glutSolidCube(0.5);
 				glTranslatef(-0.5,0,0);
 			}
 			glTranslatef(0.5*(float)x-0.5,0,0.5);
-		}else if( z<550 ){
+		}
+
+		// straight section (80)
+		else if( z<550 ){
 			if( z == 470 )
 				glTranslatef(0,0,0.5);
 			for( x=0; x<10; x++ ){
@@ -86,14 +109,20 @@ void Track::draw(){
 				glTranslatef(0,0,0.5);
 			}
 			glTranslatef(-0.5,0,-5);
-		}else if( z<560 ){
+		}
+
+		// fourth turn (left,10)
+		else if( z<560 ){
 			glColor3f(0,0.5,0.5);
 			for( x=0; x<560-z; x++ ){
 				glutSolidCube(0.5);
 				glTranslatef(0,0,0.5);
 			}
 			glTranslatef(-0.5,0,-0.5*(float)x+0.5);
-		}else{
+		}
+
+		// final straight section (100)
+		else{
 			if( z == 560 )
 				glTranslatef(0.5,0,0);
 			for( x=0; x<10; x++ ){
