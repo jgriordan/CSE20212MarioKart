2e3af0386e86448edc3a45a23c75521a2a1c48f4
Andrew Lubera <alubera@nd.edu>
2015-04-28 00:56:43 -0400

    Starting menu functionality added, still needs fixing

--- main.cpp
+++ main.cpp
@@ -17,11 +17,17 @@ float angle=90.;
 
 int width = 960, height = 720;
 
+// control track and kart choices
+// also control flow from menus to gameplay
+int trackChoice = 0;
+int kartChoice = 0;
+// need another controlling variable to make sure that gameplay is started after track and kart are properly instantiated
+int startGame = 0;
+
 
 int myrandom( int m ){
 	return rand() % m;
 }
-
 
 void idle(){
 	glutPostRedisplay();
@@ -32,40 +38,64 @@ void processNormalKeys(unsigned char key, int x, int y) {
 	if( key == 27 ){
 		exit(0);
 	}
-	// use spacebar to activate mushroom
-	else if( key == 32 ){
-		uKart->useShroom();
-	}	
+	if (startGame) {
+		// use spacebar to activate mushroom
+		if( key == 32 ){
+			uKart->useShroom();
+		}
+	}
+	if (!trackChoice) {
+		if (key == 49) {
+			trackChoice = 1;
+			std::cout << trackChoice << std::endl;
+		} else if (key == 50) {
+			trackChoice = 2;
+			std::cout << trackChoice << std::endl;
+		}
+	} else {
+		if (key == 49) {
+			kartChoice = 1;
+			std::cout << kartChoice << std::endl;
+			std::cout << startGame << std::endl;
+		} else if (key == 50) {
+			kartChoice = 2;
+			std::cout << kartChoice << std::endl;
+		} else if (key == 51) {
+			kartChoice = 3;
+			std::cout << kartChoice << std::endl;
+		}
+	}
 }
 
 void processSpecialKeys( int key, int x, int y ){
-	switch(key){
-	case GLUT_KEY_RIGHT:
-		angle -= 1;
-		uKart->setAngle(angle);
-		if( uKart->getSpeed() > 0 )
-			uKart->setSpeed( uKart->getSpeed() - 0.01 );
-		else if( uKart->getSpeed() < 0 )
-			uKart->setSpeed( uKart->getSpeed() + 0.01 ); 
-		break;
-	case GLUT_KEY_LEFT:
-		angle += 1;
-		uKart->setAngle(angle);
-		if( uKart->getSpeed() > 0 )
-			uKart->setSpeed( uKart->getSpeed() - 0.01 );
-		else if( uKart->getSpeed() < 0 )
-			uKart->setSpeed( uKart->getSpeed() + 0.01 ); 
-		break;
-	case GLUT_KEY_UP:
-		if( uKart->getSpeed() < 4 )
-			uKart->setSpeed( uKart->getSpeed() + 0.04 - (uKart->getSpeed()/100) );
-		break;
-	case GLUT_KEY_DOWN:
-		if( uKart->getSpeed() > -4 )
-			uKart->setSpeed( uKart->getSpeed() - ( 0.04 + (uKart->getSpeed()/100) ) );
-		break;
+	if (startGame) {
+		switch(key){
+		case GLUT_KEY_RIGHT:
+			angle -= 1;
+			uKart->setAngle(angle);
+			if( uKart->getSpeed() > 0 )
+				uKart->setSpeed( uKart->getSpeed() - 0.01 );
+			else if( uKart->getSpeed() < 0 )
+				uKart->setSpeed( uKart->getSpeed() + 0.01 ); 
+			break;
+		case GLUT_KEY_LEFT:
+			angle += 1;
+			uKart->setAngle(angle);
+			if( uKart->getSpeed() > 0 )
+				uKart->setSpeed( uKart->getSpeed() - 0.01 );
+			else if( uKart->getSpeed() < 0 )
+				uKart->setSpeed( uKart->getSpeed() + 0.01 ); 
+			break;
+		case GLUT_KEY_UP:
+			if( uKart->getSpeed() < 4 )
+				uKart->setSpeed( uKart->getSpeed() + 0.04 - (uKart->getSpeed()/100) );
+			break;
+		case GLUT_KEY_DOWN:
+			if( uKart->getSpeed() > -4 )
+				uKart->setSpeed( uKart->getSpeed() - ( 0.04 + (uKart->getSpeed()/100) ) );
+			break;
+		}
 	}
-
 }
 
 void OnReshape( int w, int h ){
@@ -79,101 +109,177 @@ void OnReshape( int w, int h ){
 }
 
 void OnDraw(){
-	glClear( GL_DEPTH_BUFFER_BIT|GL_COLOR_BUFFER_BIT );
-	glColor3f( 0, 1, 1 );
-	glShadeModel( GL_FLAT );
-	glEnable( GL_LIGHTING ); 
-	glEnable( GL_LIGHT0 );
-	glEnable( GL_COLOR_MATERIAL );
-	float ambient[] = {1, 0, 0, 1}; 
-	glLightModelfv(GL_LIGHT_MODEL_AMBIENT, ambient);
-	glLoadIdentity();
-	if( uKart->getSpeed() > 0 )
-		uKart->setSpeed( uKart->getSpeed() - 0.02 );
-	else if( uKart->getSpeed() < 0 )
-                uKart->setSpeed( uKart->getSpeed() + 0.02 );
-	uKart->setLocation( uKart->getX() - uKart->getSpeed() * cos( uKart->getAngle() ), uKart->getY(), uKart->getZ() + uKart->getSpeed() * sin( uKart->getAngle() ) );
-	myC.KartLocation( uKart->getX(), uKart->getY(), uKart->getZ(), uKart->getAngle() );// set camera to behind kart
-	myC.updateLookAt();// update camera drawing
-	uKart->time();
-	uKart->lapFunc();
-	uKart->wrongWay();
-	glPushMatrix();
-	glTranslatef( uKart->getX(), uKart->getY(), uKart->getZ() );// move to kart location
-	glRotatef( -90, 0, 1, 0 );// rotate so the kart faces forward initially
-	glRotatef( angle - 90, 0, 1, 0 );// rotate the kart to the apropriate angle
-	uKart->DrawKart();
-	glPopMatrix();
-	myTrack->draw();
+	if (startGame) {
+		glClear( GL_DEPTH_BUFFER_BIT|GL_COLOR_BUFFER_BIT );
+		glColor3f( 0, 1, 1 );
+		glShadeModel( GL_FLAT );
+		glEnable( GL_LIGHTING ); 
+		glEnable( GL_LIGHT0 );
+		glEnable( GL_COLOR_MATERIAL );
+		float ambient[] = {1, 0, 0, 1}; 
+		glLightModelfv(GL_LIGHT_MODEL_AMBIENT, ambient);
+		glLoadIdentity();
+		if( uKart->getSpeed() > 0 )
+			uKart->setSpeed( uKart->getSpeed() - 0.02 );
+		else if( uKart->getSpeed() < 0 )
+			uKart->setSpeed( uKart->getSpeed() + 0.02 );
+		uKart->setLocation( uKart->getX() - uKart->getSpeed() * cos( uKart->getAngle() ), uKart->getY(), uKart->getZ() + uKart->getSpeed() * sin( uKart->getAngle() ) );
+		myC.KartLocation( uKart->getX(), uKart->getY(), uKart->getZ(), uKart->getAngle() );// set camera to behind kart
+		myC.updateLookAt();// update camera drawing
+		uKart->time();
+		uKart->lapFunc();
+		uKart->wrongWay();
+		glPushMatrix();
+		glTranslatef( uKart->getX(), uKart->getY(), uKart->getZ() );// move to kart location
+		glRotatef( -90, 0, 1, 0 );// rotate so the kart faces forward initially
+		glRotatef( angle - 90, 0, 1, 0 );// rotate the kart to the apropriate angle
+		uKart->DrawKart();
+		glPopMatrix();
+		myTrack->draw();
 
-	// display time to the window
-	glMatrixMode(GL_PROJECTION);
-	glPushMatrix();
-	glLoadIdentity();
-	gluOrtho2D(0,width,height,0);
-	glMatrixMode(GL_MODELVIEW);
-	glPushMatrix();
-	glLoadIdentity();
-	glDisable(GL_LIGHTING);
-	glColor3f(0.9,1.0,1.0);
-	glRasterPos2f(10,20);
-	std::stringstream s;
-	s << "LAP: " << uKart->getLap_n()+1;
-	std::string text = s.str();
-	for (int i = 0; i < text.size(); ++i) {
-		glutBitmapCharacter(GLUT_BITMAP_TIMES_ROMAN_24,text[i]);
-	}
-	s.str("");
-	text.clear();
-	glRasterPos2f(10,50);
-	s << "TIME: " << (int)uKart->getLap_t();
-	text = s.str();
-	for (int i = 0; i < text.size(); ++i) {
-		glutBitmapCharacter(GLUT_BITMAP_TIMES_ROMAN_24,text[i]);
-	}
-	s.str("");
-	text.clear();
-	glRasterPos2f(10,80);
-	s << "SPEED: " << (int)(uKart->getSpeed()*25) << " mph";
-	text = s.str();
-	for (int i = 0; i < text.size(); ++i) {
-		glutBitmapCharacter(GLUT_BITMAP_TIMES_ROMAN_24,text[i]);
-	}
-	s.str("");
-	text.clear();
-	glRasterPos2f(10,110);
-	s << "BEST: " << uKart->getBestTime();
-	text = s.str();
-	for (int i = 0; i < text.size(); ++i) {
-		glutBitmapCharacter(GLUT_BITMAP_TIMES_ROMAN_24,text[i]);
-	}
-	if( uKart->getLap_t() < 5 && uKart->getBestTime() != "-" ){
+		// display time to the window
+		glMatrixMode(GL_PROJECTION);
+		glPushMatrix();
+		glLoadIdentity();
+		gluOrtho2D(0,width,height,0);
+		glMatrixMode(GL_MODELVIEW);
+		glPushMatrix();
+		glLoadIdentity();
+		glDisable(GL_LIGHTING);
+		glColor3f(0.9,1.0,1.0);
+		glRasterPos2f(10,20);
+		std::stringstream s;
+		s << "LAP: " << uKart->getLap_n()+1;
+		std::string text = s.str();
+		for (int i = 0; i < text.size(); ++i) {
+			glutBitmapCharacter(GLUT_BITMAP_TIMES_ROMAN_24,text[i]);
+		}
 		s.str("");
 		text.clear();
-		glRasterPos2f(10,140);
-		s << "LAP TIME: " << uKart->getLastTime();
+		glRasterPos2f(10,50);
+		s << "TIME: " << (int)uKart->getLap_t();
 		text = s.str();
 		for (int i = 0; i < text.size(); ++i) {
 			glutBitmapCharacter(GLUT_BITMAP_TIMES_ROMAN_24,text[i]);
 		}
-	}
-	if (uKart->wrongWay() == 1)
-	{
 		s.str("");
 		text.clear();
-		glRasterPos2f(10, 170);
-		s << "WRONG WAY!";
+		glRasterPos2f(10,80);
+		s << "SPEED: " << (int)(uKart->getSpeed()*25) << " mph";
 		text = s.str();
-		for (int i = 0; i < text.size(); ++i)
-			glutBitmapCharacter(GLUT_BITMAP_TIMES_ROMAN_24, text[i]);
+		for (int i = 0; i < text.size(); ++i) {
+			glutBitmapCharacter(GLUT_BITMAP_TIMES_ROMAN_24,text[i]);
+		}
+		s.str("");
+		text.clear();
+		glRasterPos2f(10,110);
+		s << "BEST: " << uKart->getBestTime();
+		text = s.str();
+		for (int i = 0; i < text.size(); ++i) {
+			glutBitmapCharacter(GLUT_BITMAP_TIMES_ROMAN_24,text[i]);
+		}
+		if( uKart->getLap_t() < 5 && uKart->getBestTime() != "-" ){
+			s.str("");
+			text.clear();
+			glRasterPos2f(10,140);
+			s << "LAP TIME: " << uKart->getLastTime();
+			text = s.str();
+			for (int i = 0; i < text.size(); ++i) {
+				glutBitmapCharacter(GLUT_BITMAP_TIMES_ROMAN_24,text[i]);
+			}
+		}
+		if (uKart->wrongWay() == 1)
+		{
+			s.str("");
+			text.clear();
+			glRasterPos2f(10, 170);
+			s << "WRONG WAY!";
+			text = s.str();
+			for (int i = 0; i < text.size(); ++i)
+				glutBitmapCharacter(GLUT_BITMAP_TIMES_ROMAN_24, text[i]);
+		}
+		glPopMatrix();
+		glMatrixMode(GL_PROJECTION);
+		glPopMatrix();
+		glMatrixMode(GL_MODELVIEW);
+	} else {
+		glClear( GL_DEPTH_BUFFER_BIT|GL_COLOR_BUFFER_BIT );
+		// display menu to the window
+		glMatrixMode(GL_PROJECTION);
+		glPushMatrix();
+		glLoadIdentity();
+		gluOrtho2D(0,width,height,0);
+		glMatrixMode(GL_MODELVIEW);
+		glPushMatrix();
+		glLoadIdentity();
+		glDisable(GL_LIGHTING);
+		glColor3f(0.9,1.0,1.0);
+		glRasterPos2f(300,300);
+		std::stringstream s;
+		s << "WELCOME TO MARIO KART";
+		std::string text = s.str();
+		for (int i = 0; i < text.size(); ++i) {
+			glutBitmapCharacter(GLUT_BITMAP_TIMES_ROMAN_24,text[i]);
+		}
+		s.str("");
+		text.clear();
+		if (!trackChoice) {
+			glRasterPos2f(300,400);
+			text = "Choose your track:";
+			for (int i = 0; i < text.size(); ++i) {
+				glutBitmapCharacter(GLUT_BITMAP_TIMES_ROMAN_24,text[i]);
+			}
+			s.str("");
+			text.clear();
+			glRasterPos2f(310,430);
+			text = "Easy Track - 1";
+			for (int i = 0; i < text.size(); ++i) {
+				glutBitmapCharacter(GLUT_BITMAP_TIMES_ROMAN_24,text[i]);
+			}
+			s.str("");
+			text.clear();
+			glRasterPos2f(310,460);
+			text = "Hard Track - 2";
+			for (int i = 0; i < text.size(); ++i) {
+				glutBitmapCharacter(GLUT_BITMAP_TIMES_ROMAN_24,text[i]);
+			}
+			s.str("");
+			text.clear();
+		} else {
+			glRasterPos2f(300,400);
+			text = "Choose your kart:";
+			for (int i = 0; i < text.size(); ++i) {
+				glutBitmapCharacter(GLUT_BITMAP_TIMES_ROMAN_24,text[i]);
+			}
+			s.str("");
+			text.clear();
+			glRasterPos2f(310,430);
+			text = "Teapot - 1";
+			for (int i = 0; i < text.size(); ++i) {
+				glutBitmapCharacter(GLUT_BITMAP_TIMES_ROMAN_24,text[i]);
+			}
+			s.str("");
+			text.clear();
+			glRasterPos2f(310,460);
+			text = "Cow - 2";
+			for (int i = 0; i < text.size(); ++i) {
+				glutBitmapCharacter(GLUT_BITMAP_TIMES_ROMAN_24,text[i]);
+			}
+			s.str("");
+			text.clear();
+			glRasterPos2f(310,490);
+			text = "Snowman - 3";
+			for (int i = 0; i < text.size(); ++i) {
+				glutBitmapCharacter(GLUT_BITMAP_TIMES_ROMAN_24,text[i]);
+			}
+			s.str("");
+			text.clear();
+
+		}
 	}
-	glPopMatrix();
-	glMatrixMode(GL_PROJECTION);
-	glPopMatrix();
-	glMatrixMode(GL_MODELVIEW);
 	glutSwapBuffers();
 }
-
+	
 // do nothing on exit (for now)
 void OnExit(){}
 
@@ -192,16 +298,20 @@ int main( int argc, char** argv ){
 	glutSpecialFunc( processSpecialKeys );
 	Track1 myTrack1;
 	Track2 myTrack2;
-	int trackChoice = 1;  // to be replaced with user input
-	if (trackChoice == 1)	
-		myTrack = &myTrack1;
-	else if (trackChoice == 2)
-		myTrack = &myTrack2;
-//	myTrack = &myTrack1;
-//	myTrack = &myTrack2;
-	uKart = new Kart( 1, myTrack, trackChoice);
-	uKart->setAngle(angle);
-	uKart->setInitialTime();
+	// program will seg fault without conditional
+	if (kartChoice) {
+		if (trackChoice == 1)	
+			myTrack = &myTrack1;
+		else if (trackChoice == 2)
+			myTrack = &myTrack2;
+		std::cout << kartChoice << std::endl << trackChoice << std::cout;
+		uKart = new Kart( kartChoice, myTrack, trackChoice);
+		uKart->setAngle(angle);
+		uKart->setInitialTime();
+		std::cout << "kart chosen" << std::endl;
+		startGame = 1;
+	}
+
 	glutMainLoop();
 	return 0;
 }
